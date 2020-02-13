ff29a84a5d73df6aa16dd7c060252e7a7ebb5b4f
Matheus <matheus08.pimentel@gmail.com>
2020-02-13 13:12:41 -0300

    Create basic axios structure

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-alert('Opa');
+import axios from 'axios';
+
+class Api {
+  static async getUserInfo(username){
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+
+    const followers = response.data.followers;
+    const following = response.data.following;
+    const avatar = response.data.avatar_url;
+
+    console.log(response);
+  }
+
+  static async getUserRepo(username){
+    const response = await axios.get(`https://api.github.com/users/${username}/repos`);
+
+    const repo = response.data.length;
+    console.log(repo);
+  }
+}
+
+Api.getUserInfo('matheuscpimentel');
+Api.getUserRepo('matheuscpimentel');
