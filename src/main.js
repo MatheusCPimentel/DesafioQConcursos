574fafb0ff0773e27f2ad0dc491aefb81263f2a7
MatheusPimentel <matheus08.pimentel@gmail.com>
2020-02-13 18:05:56 -0300

    Add cover and update the structure

--- src/main.js
+++ src/main.js
@@ -7,17 +7,27 @@ class Api {
     const followers = response.data.followers;
     const following = response.data.following;
     const avatar = response.data.avatar_url;
+    const repos = response.data.public_repos
+
+    console.log(`Seguidores: ${followers}`);
+    console.log(`Seguindo: ${following}`);
+    console.log(`URL avatar: ${avatar}`);
+    console.log(`Repositórios: ${repos}`);
 
     console.log(response);
   }
 
   static async getUserRepo(username){
     const response = await axios.get(`https://api.github.com/users/${username}/repos`);
+  }
 
-    const repo = response.data.length;
-    console.log(repo);
+  static async getUserStarred(username){
+    const response = await axios.get(`https://api.github.com/users/${username}/starred`);
+
+    console.log(response.data);
   }
 }
 
 Api.getUserInfo('matheuscpimentel');
 Api.getUserRepo('matheuscpimentel');
+Api.getUserStarred('matheuscpimentel');
