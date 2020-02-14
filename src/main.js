6aa2acfdf0d37bc23e524eef3ff9cb5e5c5d88a8
Matheus <matheus08.pimentel@gmail.com>
2020-02-14 18:15:03 -0300

    Add input GitHub and GitHub Profile Card

--- src/main.js
+++ src/main.js
@@ -1,47 +1,86 @@
-class Api {
-  static async getUserInfo(username){
-    fetch(`https://api.github.com/users/${username}`).then((response) => {
+// Requisições a API do GitHub:
+
+// Profile:
+
+async function gitProfile() {
+  await fetch("https://api.github.com/users/qcx").then(response => {
+      if (!response.ok) {
+          throw Error('err');
+      } 
       return response.json();
-    })
-    .then((userInfo) => {
-
-      const avatar = userInfo.avatar_url; 
-      const followers = userInfo.followers;
-      const following = userInfo.following;
-      const repos = userInfo.public_repos;
-
-      console.log(avatar);
-      console.log(followers);
-      console.log(following);
-      console.log(repos);
-    });
-  }
-
-   static async getUserRepo(username){
-    fetch(`https://api.github.com/users/${username}/repos`).then((response) => {
-      return response.json();
-    })
-    .then((userRepo) => {
-
-      
-
-      console.log(userRepo);
-    });
-  }
-
-  static async getUserStarred(username){
-    fetch(`https://api.github.com/users/${username}/starred`).then((response) => {
-      return response.json();
-    })
-    .then((userStarred) => {
-
-
-
-      console.log(userStarred);
-    });
-  }
+  }).then(data => {
+      console.log(data);
+      renderGitCard(data);
+  }).catch(err => {
+      console.log(err);
+  });
 }
 
-Api.getUserInfo('matheuscpimentel');
-Api.getUserRepo('matheuscpimentel');
-Api.getUserStarred('matheuscpimentel');
+
+// Repositório:
+
+async function gitRepo() {
+  await fetch("https://api.github.com/users/qcx/repos").then(response => {
+      if (!response.ok) {
+          throw Error('err')
+      }
+      return response.json()
+  }).then(data => {
+      renderRepo(data)
+  }).catch(err => {
+      console.log(err)
+  })
+}
+
+
+// Favoritos:
+
+async function gitStarred() {
+  await fetch("https://api.github.com/users/qcx/starred").then(response => {
+      if (!response.ok) {
+          throw Error('err')
+      }
+      return response.json()
+  }).then(data => {
+      renderStarred(data)
+  }).catch(err => {
+      console.log(err)
+  })
+}
+
+gitProfile();
+gitRepo();
+gitStarred();
+
+
+// Renderizar Desafios:
+
+function renderGitCard(data) {
+  const divProfile = document.getElementById('profile');
+  const resHtml = 
+  ` <h1>${data.name}</h1>
+  
+    <div class="container-gitRepo">
+      <div class="left">
+          <img src="${data.avatar_url}">
+          <a href="${data.html_url}" target="_blank">VISITAR PERFIL</a>
+      </div>
+
+      <div class="right">
+        <ul>
+          <li>REPOSITÓRIOS: ${data.public_repos}</li>
+          <li>SEGUIDORES: ${data.followers}</li>
+          <li>SEGUINDO: ${data.following}</li>
+        </ul>
+          <div class="btn">
+            <input type="button" value="VER REPOSITÓRIOS" id="btn-rep" onclick="showRepo()"></input>
+            <input type="button" value="VER FAVORITOS" id="btn-starred" onclick="showStarred()"></input>
+          </div>
+        </div>
+     </div>
+
+    <div id="repo-list"></div>
+    </div> `;
+
+  divProfile.innerHTML = resHtml;
+}
